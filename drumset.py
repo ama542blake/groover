91f7d205685e5c272b2d473485aaa9275644d0ac
Blake A Feucht <ama542blake@gmail.com>
2023-02-19 21:50:30 -0500

    Progress on generator

--- drumset.py
+++ drumset.py
@@ -1,22 +1,74 @@
-from typing import Optional
+from typing import Dict, Optional, Tuple
 from music21.instrument import UnpitchedPercussion
 from music21.note import Unpitched, Rest
 from music21.duration import Duration
 
-class DrumSet:
-    def snare(quarter_length: Optional[float] = None) -> Unpitched:
-        return Unpitched('C5', duration=Duration(quarter_length))
+# class DrumSet:
+#     def snare(quarter_length: Optional[float] = None) -> Unpitched:
+#         return Unpitched('C5', duration=Duration(quarter_length))
 
 
-    def hh(quarter_length: Optional[float] = None) -> Unpitched:
-        hh = Unpitched('G5')
-        hh.notehead = "x"
-        return hh
+#     def hh(quarter_length: Optional[float] = None) -> Unpitched:
+#         hh = Unpitched('G5')
+#         hh.notehead = "x"
+#         return hh
 
 
-    def kick(quarter_length: Optional[float] = None) -> Unpitched:
-        return Unpitched('F4')
+#     def kick(quarter_length: Optional[float] = None) -> Unpitched:
+#         return Unpitched('F4')
 
 
-    def rest(quarter_length: Optional[float] = None) -> Rest:
-        return Rest()
+#     def rest(quarter_length: Optional[float] = None) -> Rest:
+#         return Rest()
+
+# https://web.mit.edu/music21/doc/moduleReference/moduleNote.html#music21.note.NotRest.notehead
+NOTE_HEAD_NORMAL = 'normal'
+NOTE_HEAD_X = 'x'
+NOTE_HEAD_CIRCLED_X = 'circle-x'
+NOTE_HEAD_TRIANGLE = 'triangle'
+
+# pair of (staff position, note head)
+Notation = Tuple[str, str]
+# TODO: verify correctness
+descriptor_to_notation: Dict[str, Notation] = {
+    'h': ('F5', NOTE_HEAD_X),
+    'H': ('F5', NOTE_HEAD_CIRCLED_X),
+    'c': ('A5', NOTE_HEAD_X),
+    'C': ('A5', NOTE_HEAD_CIRCLED_X),
+    'r': ('G5', NOTE_HEAD_CIRCLED_X),
+    'R': ('G5', NOTE_HEAD_TRIANGLE),
+    '1': ('E5', NOTE_HEAD_NORMAL),
+    '2': ('D5', NOTE_HEAD_NORMAL),
+    '3': ('A4', NOTE_HEAD_NORMAL),
+    's': ('C5', NOTE_HEAD_NORMAL),
+    'k': ('F4', NOTE_HEAD_NORMAL)
+}
+
+available_instruments = 'hcCrRsk123'
+
+# for use in printing out instruction sets to 
+instrument_descriptors = '''
+'h' - hi-hat
+'c' - crash 1
+'C' - crash 2
+'r' - ride
+'R' - ride bell
+'s' - snare
+'k' - kick
+'1' - high tom
+'2' - mid tom
+'3' - floor tom
+'''
+
+descriptor_to_string: Dict[str, str] = {
+    'h': 'hi-hat',
+    'c': 'crash 1',
+    'C': 'crash 2',
+    'r': 'ride',
+    'R': 'ride bell',
+    's': 'snare',
+    'k': 'kick',
+    '1': 'high tom',
+    '2': 'mid tom',
+    '3': 'floor tom'
+}
--- generator.py
+++ generator.py
@@ -1,52 +1,52 @@
 # see Notation.mnd
-from typing import Dict, List
+from typing import Dict, List, Union
 from timesignatureutils import InvalidTimeSignatureException, TimeSignature, parse_time_signature
-
-
-available_instruments = 'hcCrRsk123'
-
-instrument_descriptors = '''
-'h' - hi-hat
-'c' - crash 1
-'C' - crash 2
-'r' - ride
-'R' - ride bell
-'s' - snare
-'k' - kick
-'1' - high tom
-'2' - mid tom
-'3' - floor tom
-'''
-
-descriptor_to_string: Dict[str, str] = {
-    'h': 'hi-hat',
-    'c': 'crash 1',
-    'C': 'crash 2',
-    'r': 'ride',
-    'R': 'ride bell',
-    's': 'snare',
-    'k': 'kick',
-    '1': 'high tom',
-    '2': 'mid tom',
-    '3': 'floor tom'
-}
+from music21.stream.base import Stream
+from music21.percussion import PercussionChord
+from drumset import available_instruments, instrument_descriptors, descriptor_to_string, descriptor_to_notation
 
 # e.g., "{'k': 'x  xx  x'}"
 InstrumentAndRhythm = Dict[str, str]
+Measure = Stream[PercussionChord]
+Groove = Stream[Measure]
+
+# each PercussionChord corresponds to each temporal position within the measure\
+# each Stream[PercussionChord] is a measure
+# the Stream[Stream[PercussionChord]] is the entire groove
+def measures_to_stream(measure_strs: List[InstrumentAndRhythm], time_signature: TimeSignature) -> Groove:
+    groove: Groove = Stream()
+    for raw_measure in measure_strs:
+        measure: Measure = Stream()
+
+        for temporal_idx in range(time_signature.measure_length):
+            
+
+        for (part, rhythm) in raw_measure:
+            (staff_position, note_head) = descriptor_to_notation[part]
+            for note in rhythm:
+                if note == ' ':
+                    measure
+
+
+
 
 def simple_generator():
     ts_valid: bool = False
+    time_sig = None
     while not ts_valid:
         try:
-            time_sig: TimeSignature = parse_time_signature(input('Enter a time signature, or hit enter for 4/4\n'))
-            ts_valid = True
+            time_sig: Union[TimeSignature, None] = parse_time_signature(input('Enter a time signature, or hit enter for 4/4\n'))
+            ts_valid = True  # parsing will fail if it's not valid
         except InvalidTimeSignatureException:
             continue
 
-    num_measures_remaining: int = 0
-    while(num_measures_remaining < 1):
+    if time_sig is None:
+        raise RuntimeError("variable 'time_sig' is None, but it should not be")
+
+    num_measures: int = 0
+    while(num_measures < 1):
         try:  # will throw exception if input is not numeric
-            num_measures_remaining: int = int(input("Enter number of measures\n"))
+            num_measures: int = int(input("Enter number of measures\n"))
         except:
             continue
 
@@ -60,14 +60,18 @@ def simple_generator():
                 to_use = ''  # will force new input
 
 
-    measure_num = 1
     measures: List[InstrumentAndRhythm] = []  # indices are measure_num - 1
-    while (num_measures_remaining > 0):
-        for descriptor in to_use:
-            # TODO: need to do some validation on this as its input (e.g., make sure it's not too long)
-            # https://docs.python.org/3/library/readline.html#module-readline
-            this_rhythm = input(f'Measure {measure_num} {descriptor_to_string[descriptor]} rhythm: ')
+    for measure_idx in range(num_measures):
+        for (descriptor_idx, descriptor) in enumerate(to_use):
+            this_rhythm = ''
+            while len(this_rhythm) != time_sig.measure_length:
+                this_rhythm = input(f'Measure {measure_idx + 1} {descriptor_to_string[descriptor]} rhythm: ')
+            if descriptor_idx == 0:
+                measures.append({descriptor: this_rhythm})
+            else:
+                measures[measure_idx][descriptor] = this_rhythm
 
-        measure_num += 1
+    measures_to_streams(measures)
+            
     
     
