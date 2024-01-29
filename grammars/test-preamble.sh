4037f7169275438b3a4840839cb1a52c6121a854
Blake A Feucht <ama542blake@gmail.com>
2024-01-28 22:32:38 -0500

    Add preamble test file

--- grammars/test-preamble.sh
+++ grammars/test-preamble.sh
@@ -1,13 +1,34 @@
+OPTIND=1
+START_RULE=preamble
+
+while getopts ":s:t:" opt ; do
+    case $opt in
+        t)
+            TEST_FILE="$(pwd)/${OPTARG}"
+            echo "Parsing ${TEST_FILE}..."
+            ;;
+        s)
+            START_RULE=$OPTARG
+            echo "Start with rule ${START_RULE}"
+            ;;
+        ?)
+            echo "Invalid option: -${opt}."
+            exit -1
+            ;;
+        :)
+            echo "Option -${opt} requires an argument."
+            exit -1
+            ;;
+    esac
+done
+
 # move to the directory the grammar exists in
 cd testable/preamble
 
-# check whether rule name to test was specified
-if [[ $1 == "" ]]
-then
-    grun Preamble preamble -gui
-else
-    grun Preamble $1 -gui
-fi
+# takes input from std in if there's no test file specified
+grun Preamble $START_RULE -gui $TEST_FILE
 
 # // return to directory the script was run from
 cd ../..
+unset START_RULE
+unset TEST_FILE
